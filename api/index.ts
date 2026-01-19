1de27487d4eac110d3f520522baa0034294c7afa
karthik6351 <gonuguntakarthik123@gmail.com>
2026-01-19 12:09:56 +0530

    Fix Vercel rewrite to point to index.ts and add logging

--- api/index.ts
+++ api/index.ts
@@ -11,6 +11,10 @@ dotenv.config();
 const app = express();
 
 // Middleware
+app.use((req, res, next) => {
+    console.log(`[API] ${req.method} ${req.url}`);
+    next();
+});
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
