dfdb19d0117244e94d0d0b0cac0f6bc566f4766d
karthik6351 <gonuguntakarthik123@gmail.com>
2026-01-19 15:22:30 +0530

    update

--- api/index.ts
+++ api/index.ts
@@ -16,8 +16,8 @@ app.use((req, res, next) => {
     next();
 });
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 connectDB();
 
