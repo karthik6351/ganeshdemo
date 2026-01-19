13a5cf15feb24b3334e97f84ba1668f76c8f5c54
karthik6351 <gonuguntakarthik123@gmail.com>
2026-01-19 11:41:22 +0530

    Remove legacy files causing build errors

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css' // Tailwind
 import './i18n' // I18n init
-import { registerSW } from 'virtual:pwa-register'
-
-// Auto-update service worker
-registerSW({ immediate: true })
+// registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
